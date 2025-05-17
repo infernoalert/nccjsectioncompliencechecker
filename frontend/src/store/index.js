d6ed3a0b35671d1abebc532791ccfdc0e96eb889
Payam <payam.a@firealertnow.com>
2025-05-17 15:08:06 +1000

    update and addJ6

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,6 +4,7 @@ import userReducer from './slices/userSlice';
 import projectReducer from './slices/projectSlice';
 import EnergyMonitorReducer from './slices/energy_monitor_reportSlice';
 import J7LightingReducer from './slices/j7lighting_reportSlice';
+import J6hvacReducer from './slices/j6hvac_reportSlice';
 
 export const store = configureStore({
   reducer: {
@@ -12,5 +13,6 @@ export const store = configureStore({
     project: projectReducer,
     EnergyMonitor: EnergyMonitorReducer,
     J7Lighting: J7LightingReducer,
+    J6hvac: J6hvacReducer,
   },
 });
